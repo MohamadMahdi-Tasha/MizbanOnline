5c92825a0fc58441fac2cccc417669debde1af42
Mahdi Tasha <imwhdiiii@gmail.com>
2023-07-07 12:57:00 +0330

    added some configs

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
+// Importing part
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import './assets/fonts/Yekan-Bakh/font.css';
 import './index.css';
+import HomePage from "./pages/homePage.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+// Variables
+const rootElement = document.getElementById('react-root') as HTMLElement;
+const root = ReactDOM.createRoot(rootElement);
+
+// Rendering all pages in strict mode of React
+root.render(
   <React.StrictMode>
-    <h1>Hello</h1>
-  </React.StrictMode>,
+    <BrowserRouter>
+        <Routes>
+            <Route path={'/'} element={<HomePage  />} />
+        </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 )
---
+++ src/pages/homePage.tsx
@@ -0,0 +1,7 @@
+// Creating functional component of home page and exporting it as default
+export default function HomePage():JSX.Element {
+    // Returning JSX
+    return (
+        <h1>خانه</h1>
+    );
+}
