48de2b4a092b937b91d99d724405a9abd5a8c455
Mahdi Tasha <imwhdiiii@gmail.com>
2023-07-07 19:10:25 +0330

    fixed small bug

--- src/components/searchDomainComponent.tsx
+++ src/components/searchDomainComponent.tsx
@@ -17,12 +17,12 @@ export default function SearchDomainComponent():JSX.Element {
                 <form action="#" className={'bg-aliceBlue rounded-[12px] px-[24px] py-[10px] flex items-center gap-[10px]'}>
                     <input className={'bg-transparent w-full outline-0 placeholder:text-[15px] placeholder:font-normal placeholder:text-outerSpace text-darkBlue'} type="text" placeholder={'دامنه خود را جستجو کنید'} required />
                     <div className={'flex gap-[5px]'}>
-                        <button className={'w-[50px] h-[50px] rounded-[11px] flex items-center justify-center bg-myGreen'}>
+                        <button className={'w-[50px] h-[50px] rounded-[11px] flex items-center justify-center bg-myGreen'} type={"button"}>
                             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M3 8H16.5C18.9853 8 21 10.0147 21 12.5C21 14.9853 18.9853 17 16.5 17H3M3 8L6 5M3 8L6 11" stroke="white" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
                             </svg>
                         </button>
-                        <button className={'w-[50px] h-[50px] rounded-[11px] flex items-center justify-center bg-themeBlue'}>
+                        <button className={'w-[50px] h-[50px] rounded-[11px] flex items-center justify-center bg-themeBlue'} type={"button"}>
                             <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M13.8938 13.8676L17.5 17.5M15.8333 9.16667C15.8333 12.8486 12.8486 15.8333 9.16667 15.8333C5.48477 15.8333 2.5 12.8486 2.5 9.16667C2.5 5.48477 5.48477 2.5 9.16667 2.5C12.8486 2.5 15.8333 5.48477 15.8333 9.16667Z" stroke="white" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
                             </svg>
