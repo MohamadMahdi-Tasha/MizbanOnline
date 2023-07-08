51c6981d7c8f65142d5694acba3045745c07358a
Mahdi Tasha <imwhdiiii@gmail.com>
2023-07-08 07:49:32 +0330

    added styles of fourth section

--- src/components/securityFeaturesComponent.tsx
+++ src/components/securityFeaturesComponent.tsx
@@ -8,9 +8,9 @@ interface typeOfProps {
 export default function SecurityFeaturesComponent({title, children}:typeOfProps):JSX.Element {
     // Returning JSX
     return (
-        <li>
-            <span>{title}</span>
-            <span>{children}</span>
+        <li className={'relative pr-[66px] before:absolute before:bg-[url("./assets/img/homePage/fourthSection/img-tick.svg")] before:w-[45px] before:h-[45px] before:right-0 before:top-[50%] before:translate-y-[-50%]'}>
+            <span className={'block text-[16px] text-outerSpace font-normal mb-[3px]'}>{title}</span>
+            <span className={'block text-[13px] text-raven font-normal'}>{children}</span>
         </li>
     );
 }
--- src/pageSections/homePage/fourthSectionComponent.tsx
+++ src/pageSections/homePage/fourthSectionComponent.tsx
@@ -8,13 +8,17 @@ export default function FourthSectionComponent():JSX.Element {
     // Returning JSX
     return (
         <div className={'bg-myGrey'}>
-            <SectionComponent>
-                <main>
+            <SectionComponent className={'py-[71px]'}>
+                <main className={'flex items-start gap-[64px]'}>
                     <img src={MainImage} alt="ایلوستریشن"/>
                     <div>
-                        <h3>امنیت وب سایت از نام آن مهم تر است!</h3>
-                        <p>میزبان آنلاین نهایت تلاش خود را جهت ارائه هاست مطمئن و امن می نماید تا اطمینان حاصل کند، امن ترین محیط میزبانی را برای مشتریان خود فراهم کرده است.</p>
-                        <ul>
+                        <h3 className={'text-darkBlue text-[24px] font-bold mb-[10px]'}>امنیت وب سایت از نام آن مهم تر است!</h3>
+                        <p className={'text-raven text-[16px] font-normal mb-[39px]'}>
+                            میزبان آنلاین نهایت تلاش خود را جهت ارائه هاست مطمئن و امن می نماید تا اطمینان
+                            <br/>
+                            حاصل کند، امن ترین محیط میزبانی را برای مشتریان خود فراهم کرده است.
+                        </p>
+                        <ul className={'flex flex-col gap-[21px] list-inside'}>
                             <SecurityFeaturesComponent title={'سخت افزار های Anti DDoS و UTM'}>استفاده از فایروال و UTM های سخت افزاری روی لبه شبکه در دیتاسنتر</SecurityFeaturesComponent>
                             <SecurityFeaturesComponent title={'کانفیگ های امنیتی ســــرور'}>استفاده از فایروال های نرم افزاری و Bruteforce Protection پیشرفته</SecurityFeaturesComponent>
                             <SecurityFeaturesComponent title={'گواهینامه SSL و آنتی شل'}>استفاده از گواهینامه SSL معتبر و نرم افزار های Anti Shell جهت افزایش امنیت</SecurityFeaturesComponent>
