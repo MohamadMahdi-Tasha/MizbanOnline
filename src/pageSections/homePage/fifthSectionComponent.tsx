da66bbaabb50c0679ca68f6d1e328948694100ab
Mahdi Tasha <imwhdiiii@gmail.com>
2023-07-08 08:02:46 +0330

    added styles of fifth Section

--- src/pageSections/homePage/fifthSectionComponent.tsx
+++ src/pageSections/homePage/fifthSectionComponent.tsx
@@ -8,29 +8,31 @@ import BackUpImage2 from '../../assets/img/homePage/fifthSection/img-backup2.png
 export default function FifthSectionComponent():JSX.Element {
     // Returning JSX
     return (
-        <SectionComponent className={'relative'}>
-            <main>
+        <SectionComponent className={'py-[58px]'}>
+            <main className={'flex items-center gap-[150px]'}>
                 <div>
-                    <h3>اطلاعات شما سرمایه شماست !</h3>
-                    <p>
+                    <h3 className={'text-darkBlue text-[24px] font-bold'}>اطلاعات شما سرمایه شماست !</h3>
+                    <p className={'text-raven text-[16px] font-normal mb-[82px]'}>
                         دیگر نگران از دست رفتن حتی یک بیت از اطلاعات خود نباشید.  میزبان آنلاین
                         <br/>
                         با بهره گیری سرورهای مختلف به صورت خودکار در بازه های هفتگی  از
                         <br/>
                         سایت شما پشتیبان تهیه می کند
                     </p>
-                    <div>
+                    <div className={'flex gap-[72px]'}>
                         <div>
-                            <img src={BackUpImage} alt="بک اپ"/>
-                            <span>امکان تنظیم نحوه بک آپ</span>
+                            <img className={'mb-[10px] block w-[65px]'} src={BackUpImage} alt="بک اپ"/>
+                            <span className={'text-[16px] block mb-[5px] font-normal text-outerSpace'}>امکان تنظیم نحوه بک آپ</span>
+                            <span className={'text-[13px] block font-normal text-raven'}>تنظیم زمان به صورت خودکار یا دستی</span>
                         </div>
                         <div>
-                            <img src={BackUpImage2} alt="بک اپ"/>
-                            <span>بک آپ روی سرورهای مجزا</span>
+                            <img className={'mb-[10px] block w-[65px]'} src={BackUpImage2} alt="بک اپ"/>
+                            <span className={'text-[16px] block mb-[5px] font-normal text-outerSpace'}>بک آپ روی سرورهای مجزا</span>
+                            <span className={'text-[13px] block font-normal text-raven'}>افزایش ضریب اطمینان از حفظ اطلاعات</span>
                         </div>
                     </div>
                 </div>
-                <img className={'w-full'} src={MainImage} alt="میزبان انلاین"/>
+                <img src={MainImage} alt="میزبان انلاین"/>
             </main>
         </SectionComponent>
     );
